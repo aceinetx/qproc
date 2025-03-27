567e020985aabb1c49b6222f980da762b0962f31
aceinetx <mouselin82@gmail.com>
2025-03-27 20:38:17 +0300

    Update

--- src/qvm/endian.c
+++ src/qvm/endian.c
@@ -4,27 +4,28 @@
 
 dword fromQendian(byte *bytes) {
 	dword value, i;
-
 	value = 0;
-	i = 0;
-	for (; i < 4; ++i) {
+
+	for (i = 0; i < 4; ++i) {
 		value |= ((dword)(bytes[i]) << (i * 8));
 	}
 	return value;
 }
 
 CALLEOWNS byte *toQendian(dword n) {
-	byte *bytes = malloc(4);
-	size_t i = 0;
-	for (; i < 4; ++i) {
+	byte *bytes;
+	dword i;
+
+	bytes = malloc(4);
+	for (i = 0; i < 4; ++i) {
 		bytes[i] = (n >> (i * 8)) & 0xFF;
 	}
 	return bytes;
 }
 
 void ftoQendian(FILE *fd, dword n) {
-	size_t i = 0;
-	for (; i < 4; ++i) {
+	size_t i;
+	for (i = 0; i < 4; ++i) {
 		fprintf(fd, "%c", (n >> (i * 8)) & 0xFF);
 	}
 }
